6a18b024b64cdf434006b081538cb47378f4b0f5
msnkr <mikyle.designs@gmail.com>
2022-06-03 14:03:09 +0200

    .

--- tycole_web/tycole_web/settings.py
+++ tycole_web/tycole_web/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+# Load Secret keys 
+from dotenv import load_dotenv
+load_dotenv()
+
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1@h#uork&61g=9)scrw&su&8i^3t186pv)m+wue0_b-)zv2kr4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
