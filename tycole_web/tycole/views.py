2ab881c3040b7583e6a82ea0a86e3944c0b3df7d
msnkr <mikyle.designs@gmail.com>
2022-06-07 10:48:01 +0200

    .

--- tycole_web/tycole/views.py
+++ tycole_web/tycole/views.py
@@ -1,5 +1,7 @@
+import os
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from tycole.forms import RequestContactForm
 
 # Create your views here.
@@ -21,6 +23,12 @@ def contactform(request):
         form = RequestContactForm(request.POST)
         if form.is_valid():
             form.save()
+            subject = form.cleaned_data['name']
+            message = form.cleaned_data['message']
+            from_email = form.cleaned_data['email']
+            number = form.cleaned_data['number']
+            form_message = f'MESSAGE: {message}\n\nFROM: {from_email}\n\nNUMBER: {number}'
+            send_mail(subject, form_message, os.getenv('EMAIL_USERNAME'), [os.getenv('EMAIL')])
             return thank_you(request)
         else:
             raise ValidationError('Error')
@@ -29,4 +37,7 @@ def contactform(request):
 
 def thank_you(request):
     return render(request, 'tycole/thank_you.html')
+
+
+
 
